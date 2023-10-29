4dbf69ac5ab82e6551e285a3d2c431517902c2fb
bmamaxim <bma@gmail.com>
2023-10-29 21:12:32 +0500

    newfunc

--- str_func.py
+++ str_func.py
@@ -2,3 +2,12 @@
 def str_func(word: str) -> str:
     return word.upper()
 
+
+def get_initialis(txt: str):
+    """
+    Функция буквиц
+    :param txt:
+    :return:
+    """
+    return txt.title()
+
